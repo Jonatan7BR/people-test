dd3bf7b181d45aa8a1e9fe277d710c21c7da48d8
Jonatan Tiago <jonatan2206@gmail.com>
2023-01-20 16:54:43 -0300

    Adjustment

--- src/config/config.ts
+++ src/config/config.ts
@@ -11,4 +11,4 @@ export const DB_USER = env.DB_USER!;
 export const DB_PASSWORD = env.DB_PASSWORD!;
 export const DB_HOST = env.DB_HOST!;
 export const DB_PORT = +env.DB_PORT!;
-export const DB_DIALECT = <Dialect>env.DB_DIALECT;
+export const DB_DIALECT = env.DB_DIALECT as Dialect;
