728c771a1fbd1b8bfe9598044555d6b73557f88e
Jonatan Tiago <jonatan2206@gmail.com>
2023-02-07 09:48:36 -0300

    Create test

---
+++ src/test/controller/person.controller.spec.ts
@@ -0,0 +1,51 @@
+import { PersonController } from "../../controllers/person.controller";
+import { Person } from "../../models/person.model";
+
+describe('Testing Person controller', () => {
+    const id = 1;
+    const personReq = {
+        firstName: 'Taro',
+        lastName: 'Tanaka',
+        age: 30,
+        identifier: '12345678901',
+        email: 'tata@dom.co.jp'
+    };
+    const personRes = {
+        ...personReq,
+        id,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+    };
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('should return all people', async () => {
+        const person = Person;
+        person.findAll = jest.fn().mockReturnValue([personRes]);
+        const controller = new PersonController(person);
+
+        const result = await controller.getAllPeople();
+        expect(result).toHaveLength(1);
+        expect(result[0]).toBe(personRes);
+    });
+
+    it('should return one person', async () => {
+        const person = Person;
+        person.findOne = jest.fn().mockReturnValue(personRes);
+        const controller = new PersonController(person);
+
+        const result = await controller.getPerson(id);
+        expect(result).toBe(personRes);
+    });
+
+    it('should register a person', async () => {
+        const person = Person;
+        person.create = jest.fn().mockReturnValue(personRes);
+        const controller = new PersonController(person);
+
+        const result = await controller.createPerson(personReq);
+        expect(result).toBe(personRes);
+    });
+});
